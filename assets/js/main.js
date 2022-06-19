4cd196cd8993b16e36bbd89b5aa031a753f2f4d7
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-06-19 16:48:57 +0200

    heroku link added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault();
       console.log("Envoyé");
 
-      const response = await axios.post("http://localhost:3000/form", {
-        firstname: document.querySelector("#firstname").value,
-        lastname: document.querySelector("#lastname").value,
-        email: document.querySelector("#email").value,
-        subject: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value,
-      });
+      const response = await axios.post(
+        "https://tpk-formulaire-back-end.herokuapp.com/form",
+        {
+          firstname: document.querySelector("#firstname").value,
+          lastname: document.querySelector("#lastname").value,
+          email: document.querySelector("#email").value,
+          subject: document.querySelector("#subject").value,
+          message: document.querySelector("#message").value,
+        }
+      );
 
       console.log("La réponse envoyée: ", response);
     });
